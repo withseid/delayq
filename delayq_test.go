6934148d5dfb82196b7ac230e7c260e42c0b6439
lilh <lilh@mvalley.io>
2022-08-16 14:51:38 +0800

    [DEL] All Test

--- delayq_test.go
+++ delayq_test.go
@@ -1,16 +1,5 @@
 package delayq
 
-import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"math/rand"
-	"testing"
-	"time"
-
-	uuid "github.com/satori/go.uuid"
-)
-
 // func TestMigrate(t *testing.T) {
 // 	config := RedisConfiguration{
 // 		Host: "192.168.89.160",
@@ -33,71 +22,71 @@ import (
 // 	fmt.Println("jobIDs: ", jobIDs)
 // }
 
-func TestTime(t *testing.T) {
-	now := time.Now()
-	fmt.Println(now.Unix())
-}
+// func TestTime(t *testing.T) {
+// 	now := time.Now()
+// 	fmt.Println(now.Unix())
+// }
 
-func TestDelayQServer(t *testing.T) {
-	config := RedisConfiguration{
-		Host: "192.168.89.160",
-		Port: "6379",
-	}
+// func TestDelayQServer(t *testing.T) {
+// 	config := RedisConfiguration{
+// 		Host: "192.168.89.160",
+// 		Port: "6379",
+// 	}
 
-	// space := Space{}
+// 	// space := Space{}
 
-	s := NewServer(config)
+// 	s := NewServer(config)
 
-	s.Run(context.Background())
-}
+// 	s.Run(context.Background())
+// }
 
-var spaceExpiredTopic = "space_expired"
+// var spaceExpiredTopic = "space_expired"
 
-type Space struct {
-	ID     string
-	UserID string
-	Phone  string
-}
+// type Space struct {
+// 	ID     string
+// 	UserID string
+// 	Phone  string
+// }
 
-func (s *Space) Topic() string {
-	return spaceExpiredTopic
-}
+// func (s *Space) Topic() string {
+// 	return spaceExpiredTopic
+// }
 
-func (s *Space) Execute(ctx context.Context, payload []byte) error {
+// func (s *Space) Execute(ctx context.Context, payload []byte) error {
 
-	space := Space{}
-	err := json.Unmarshal(payload, &space)
-	if err != nil {
-		return err
-	}
+// 	space := Space{}
+// 	err := json.Unmarshal(payload, &space)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	fmt.Println("[Execute] Space: ", space)
-	return nil
-}
+// 	fmt.Println("[Execute] Space: ", space)
+// 	return nil
+// }
 
-func TestNewClient(t *testing.T) {
-	config := RedisConfiguration{
-		Host: "192.168.89.160",
-		Port: "6379",
-	}
-	client := NewClient(config)
+// func TestNewClient(t *testing.T) {
+// 	config := RedisConfiguration{
+// 		Host: "192.168.89.160",
+// 		Port: "6379",
+// 	}
+// 	client := NewClient(config)
 
-	rand.Seed(time.Now().UnixNano())
-	topic := "space_expired"
-	for i := 0; i < 5000; i++ {
-		space := Space{
-			ID:     fmt.Sprintf("space%d", i),
-			UserID: fmt.Sprintf("user%d", i),
-			Phone:  fmt.Sprintf("phone%d", i),
-		}
+// 	rand.Seed(time.Now().UnixNano())
+// 	topic := "space_expired"
+// 	for i := 0; i < 5000; i++ {
+// 		space := Space{
+// 			ID:     fmt.Sprintf("space%d", i),
+// 			UserID: fmt.Sprintf("user%d", i),
+// 			Phone:  fmt.Sprintf("phone%d", i),
+// 		}
 
-		data, err := json.Marshal(space)
-		if err != nil {
-			panic(err)
-		}
-		t := rand.Int31n(300)
-		fmt.Println(t)
-		client.Enqueue(topic, fmt.Sprintf("job_%s", uuid.NewV4().String()), data, ProcessAt(time.Now().Add(time.Minute*time.Duration(t))))
-	}
+// 		data, err := json.Marshal(space)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		t := rand.Int31n(300)
+// 		fmt.Println(t)
+// 		client.Enqueue(topic, fmt.Sprintf("job_%s", uuid.NewV4().String()), data, ProcessAt(time.Now().Add(time.Minute*time.Duration(t))))
+// 	}
 
-}
+// }
